b1f56b25b6dd78940b043d8ffb67859c25e5a858
Divyanshu Soni <divyanshusoni007@gmail.com>
2022-02-22 20:07:10 +0530

    first commit

---
+++ build/precache-manifest.eca397446dde5d4050b2504c89f9b426.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f672a47e6c60dccee376affb32004483",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a359d6f2db00b2f43600",
+    "url": "/static/js/2.306c8396.chunk.js"
+  },
+  {
+    "revision": "85e97eaacfe1eb3dfc11",
+    "url": "/static/js/main.d2fa2b87.chunk.js"
+  },
+  {
+    "revision": "e15cf135dc78fdc2ad46",
+    "url": "/static/js/runtime-main.d3a0f9ba.js"
+  }
+]);
